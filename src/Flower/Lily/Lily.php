21cfb3fee515ae1f6fa0f0db8f1fa23c53b81b1e
che <reg@che.com>
2018-07-22 19:47:40 +0800

    init && commit library

---
+++ src/Flower/Lily/Lily.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Create by VSCode
+ * User: che
+ * Date: 2018/7/22
+ * Time: 19:34
+ */
+
+namespace Flower\Lily;
+
+class Lily
+{
+    public function desc()
+    {
+        echo "this is lily flower";
+    }
+}
---
+++ src/Flower/Rose/Rose.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Create by VSCode
+ * User: che
+ * Date: 2018/7/22
+ * Time: 19:34
+ */
+
+namespace Flower\Rose;
+
+class Rose
+{
+    public function desc()
+    {
+        echo "this is rose flower";
+    }
+}
